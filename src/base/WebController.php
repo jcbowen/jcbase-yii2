f82f31ebcb1f1c17e319ccebf2c631d99a6fa065
jcbowen <bowen@jiuchet.com>
2022-07-19 10:34:26 +0800

    补充 web控制器基础封装

---
+++ src/base/WebController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Jcbowen\JcbaseYii2\base;
+
+use Jcbowen\JcbaseYii2\components\BaseControllerTrait;
+use Jcbowen\JcbaseYii2\components\CurdActionTrait;
+use Jcbowen\JcbaseYii2\components\Template;
+use Jcbowen\JcbaseYii2\components\Util;
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\web\Controller;
+use yii\web\Response;
+
+/**
+ * Class WebController
+ *
+ * @author Bowen
+ * @email bowen@jiuchet.com
+ * @lasttime: 2021/12/18 1:15 上午
+ * @package Jcbowen\JcbaseYii2\base
+ */
+class WebController extends Controller
+{
+    use BaseControllerTrait;
+    use CurdActionTrait;
+
+    public function init()
+    {
+        global $_B;
+
+        parent::init();
+        define('TIMESTAMP', time());
+        define('TIME', date('Y-m-d H:i:s', TIMESTAMP));
+
+        $_B['page'] = ['title' => 'jcsoft'];
+
+        $_B['params'] = ArrayHelper::merge((array)$_B['params'], Yii::$app->params);
+    }
+
+    public function vTpl($filename, $flag = TEMPLATE_DISPLAY)
+    {
+        return (new Template)->vTpl($filename, $flag);
+    }
+
+    /**
+     * 输出json结构数据
+     *
+     * @param string|int $errCode 错误码，其中0为正确
+     * @param string $errmsg 错误信息
+     * @param mixed $data 返回内容
+     * @param array $params 补充参数
+     * @param string $returnType
+     *
+     * @return string|Response
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     * @lastTime 2021/12/18 12:18 上午
+     */
+    public function result($errCode = '0', $errmsg = '', $data = [], $params = [], $returnType = 'exit')
+    {
+        return (new Util)->result($errCode, $errmsg, $data, $params, $returnType);
+    }
+
+    public function result_r($errcode = '0', $errmsg = '', $data = [], $params = [])
+    {
+        return $this->result($errcode, $errmsg, $data, $params, 'return');
+    }
+}
---
+++ src/components/BaseControllerTrait.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *
+ * @author Bowen
+ * @email bowen@jiuchet.com
+ * @lastTime 2021/12/27 11:04 下午
+ * @package Jcbowen\JcbaseYii2\components
+ */
+
+namespace Jcbowen\JcbaseYii2\components;
+
+
+trait BaseControllerTrait
+{
+
+}
