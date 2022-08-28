eb1f40a733ec35f258c7238f50d9e8725a94bda2
jcbowen <bowen@jiuchet.com>
2022-08-29 00:28:14 +0800

    新增 console基础控制器

---
+++ src/base/ConsoleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Jcbowen\JcbaseYii2\base;
+
+use Jcbowen\JcbaseYii2\components\BaseControllerTrait;
+use Jcbowen\JcbaseYii2\components\ModelHelper;
+use Yii;
+use yii\helpers\ArrayHelper;
+use yii\web\Controller;
+
+/**
+ * Class ConsoleController
+ *
+ * @author Bowen
+ * @email bowen@jiuchet.com
+ * @lasttime: 2022/8/28 23:13
+ * @package Jcbowen\JcbaseYii2\base
+ */
+class ConsoleController extends Controller
+{
+    use BaseControllerTrait;
+    use ModelHelper;
+
+    public function init()
+    {
+        global $_B;
+
+        parent::init();
+
+        //----- 默认参数，及将参数配置写到全局变量中 -----/
+        $_B['page']   = ['title' => 'jcsoft'];
+        $_B['params'] = ArrayHelper::merge((array)$_B['params'], Yii::$app->params);
+    }
+}
