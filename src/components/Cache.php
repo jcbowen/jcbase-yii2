6f48a6551fb986926bcb407d9c0c94444636c971
jcbowen <bowen@jiuchet.com>
2022-10-05 21:53:28 +0800

    新增 动态调用方法及支持设置缓存失效

--- src/components/Cache.php
+++ src/components/Cache.php
@@ -13,27 +13,110 @@ namespace Jcbowen\JcbaseYii2\components;
  */
 class Cache
 {
-    // 前缀，推荐使用项目名避免冲突
+    // 缓存key的前缀，避免项目冲突，推荐使用项目名
     public static $prefix = 'jcbase';
 
-    public static function get($key = '')
+    // 默认缓存时间，单位秒
+    public static $expire = 0;
+
+    /**
+     * @var array
+     * - prefix: string, 缓存key的前缀，避免项目冲突，推荐使用项目名
+     * - expire: int, 默认缓存时间，单位秒
+     */
+    public $config = [];
+
+    public function __construct()
+    {
+        if (!empty($this->config)) {
+            if (!empty($this->config['prefix'])) self::$prefix = $this->config['prefix'];
+            if (!empty($this->config['expire'])) self::$expire = $this->config['expire'];
+        }
+    }
+
+    /**
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $key
+     * @return array|mixed|string
+     * @lasttime: 2022/10/5 21:24
+     */
+    public static function get(string $key = '')
     {
         return Util::redisGet(self::getKey($key));
     }
 
-    public static function set($key = '', $value = null)
+    /**
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $key
+     * @param array|string $value
+     * @param int|string $expire
+     * @return mixed
+     * @lasttime: 2022/10/5 21:25
+     */
+    public static function set(string $key = '', $value = null, $expire = null)
     {
-        return Util::redisSet(self::getKey($key), $value);
+        $expire = $expire ?? self::$expire;
+        return Util::redisSet(self::getKey($key), $value, $expire);
     }
 
-    public static function del($key)
+    /**
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $key
+     * @return mixed
+     * @lasttime: 2022/10/5 21:26
+     */
+    public static function del(string $key)
     {
         return Util::redisDel(self::getKey($key));
     }
 
-    private static function getKey($key = ''): string
+    /**
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $key
+     * @return string
+     * @lasttime: 2022/10/5 21:27
+     */
+    private static function getKey(string $key = ''): string
     {
         return 'jcsoft_' . md5(self::$prefix . '_' . $key);
+    }
+
+    // ----- 动态调用 ----- /
+    public function setPrefix(string $prefix)
+    {
+        self::$prefix = $prefix;
+    }
+
+    public function getPrefix(): string
+    {
+        return self::$prefix;
+    }
+
+    public function getValue($key)
+    {
+        return self::get($key);
+    }
+
+    public function setValue($key, $value, $expire = 0)
+    {
+        return self::set($key, $value, $expire);
+    }
+
+    public function delValue($key)
+    {
+        return self::del($key);
     }
 }
 
