32ea0e2eff3f783acdd8203518e1ecd44bf74d56
jcbowen <bowen@jiuchet.com>
2022-11-17 16:18:59 +0800

    新增 获取setValue查询结果方法

--- src/components/CurdActionTrait.php
+++ src/components/CurdActionTrait.php
@@ -808,6 +808,8 @@ trait CurdActionTrait
         if ($model->$field === $value)
             return (new Util)->result(ErrCode::SUCCESS, '值未发生改变，请确认修改内容');
 
+        $this->getSetValueRecord($model);
+
         $result = $this->toSave($model, [$field => $value]);
         if (Util::isError($result))
             return (new Util)->resultError($result);
@@ -846,6 +848,20 @@ trait CurdActionTrait
             $where[] = ['deleted_at' => $this->noTime];
 
         return $where;
+    }
+
+    /**
+     * 获取setValue查询结果
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     * @param ActiveRecord $record
+     * @return ActiveRecord
+     * @lasttime 2022/11/17 16:09
+     */
+    public function getSetValueRecord(ActiveRecord &$record): ActiveRecord
+    {
+        return $record;
     }
 
     //---------- 变更通用 ----------/
