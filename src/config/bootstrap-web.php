37ff86c4a5b4419ec0d7027b287d3330f5fc7792
jcbowen <bowen@jiuchet.com>
2023-02-14 22:56:50 +0800

    常规优化

--- src/config/bootstrap-web.php
+++ src/config/bootstrap-web.php
@@ -35,25 +35,25 @@ unset($res);
 
 header('Content-Type: text/html; charset=UTF-8');
 
-/**
- * 设置允许跨域
- *
- * @author Bowen
- * @email bowen@jiuchet.com
- * @lastTime 2021/12/26 10:30 下午
- * @param string $domain
- *
- * @return bool
- */
 if (!function_exists('allowCrossDomain')) {
-    function allowCrossDomain(string $domain): bool
+    /**
+     * 设置允许跨域的域名
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $domain 域名
+     * @return bool
+     * @lasttime: 2021/12/26 10:30 下午
+     */
+    function allowCrossDomain(string $domain = ''): bool
     {
+        if (empty($domain)) return false;
+
         header("Access-Control-Allow-Origin: $domain");
         header('Access-Control-Allow-Methods: *');
-//        header('Access-Control-Allow-Methods: GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH');
-//        header('Access-Control-Allow-Headers:*');
         header("Access-Control-Allow-Headers:Authorization, Accept, Client-Security-Token, Accept-Encoding, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, X-File-Type, Cache-Control, Origin, online_token, Referer, User-Agent, JcClient, EnvVersion, RELEASE");
-        header('Access-Control-Allow-Credentials:true');
+        header('Access-Control-Allow-Credentials:true'); // 允许跨域携带cookie
         header('Access-Control-Request-Method:OPTIONS,GET,POST');
         header('Access-Control-Expose-Headers: Authorization');
         if (strtoupper($_SERVER['REQUEST_METHOD']) == 'OPTIONS') {
