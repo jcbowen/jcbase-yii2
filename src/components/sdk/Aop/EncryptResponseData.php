5b38839e7f610ffc667ac09a8a0c0a65c708db08
jcbowen <bowen@jiuchet.com>
2023-04-14 12:36:09 +0800

    修正 文件名大小写问题

---
+++ src/components/sdk/Aop/EncryptResponseData.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ *  TODO 补充说明
+ *
+ * User: jiehua
+ * Date: 16/3/30
+ * Time: 下午8:51
+ */
+
+namespace Jcbowen\JcbaseYii2\components\sdk\Aop;
+
+class EncryptResponseData
+{
+    public $realContent;
+
+    public $returnContent;
+
+} 
---
+++ src/components/sdk/Aop/schema/ServiceSchemaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Jcbowen\JcbaseYii2\components\sdk\Aop\schema;
+
+class ServiceSchemaFactory
+{
+    public static function createAttribute($id = null, $name = null, $type = null, $valueType = null)
+    {
+        $attribute = new XMLAttribute();
+        $attribute->setId($id);
+        $attribute->setName($name);
+        $attribute->setType($type);
+        $attribute->setValueType($valueType);
+        return $attribute;
+    }
+
+    public static function createRule($type = null, $name = null, $value = null)
+    {
+        return new AttributeRule($type, $name, $value);
+    }
+
+    public static function createOption($displayName = null, $value = null)
+    {
+        return new Option($displayName, $value);
+    }
+}
