33ded690de51bd467a5fcf9e92e6239973326e75
jcbowen <bowen@jiuchet.com>
2023-04-17 14:14:22 +0800

    完善 支付宝支付SDK

--- src/components/sdk/AliPay.php
+++ src/components/sdk/AliPay.php
@@ -4,8 +4,12 @@ namespace Jcbowen\JcbaseYii2\components\sdk;
 
 use Jcbowen\JcbaseYii2\components\Content;
 use Jcbowen\JcbaseYii2\components\ErrCode;
+use Jcbowen\JcbaseYii2\components\Safe;
 use Jcbowen\JcbaseYii2\components\sdk\Aop\AopClient;
 use Jcbowen\JcbaseYii2\components\sdk\Aop\request\AlipayTradeAppPayRequest;
+use Jcbowen\JcbaseYii2\components\sdk\Aop\request\AlipayTradeCloseRequest;
+use Jcbowen\JcbaseYii2\components\sdk\Aop\request\AlipayTradeQueryRequest;
+use Jcbowen\JcbaseYii2\components\sdk\Aop\request\AlipayTradeRefundRequest;
 use Jcbowen\JcbaseYii2\components\Util;
 use stdClass;
 use Yii;
@@ -57,22 +61,17 @@ class AliPay extends Component
     /**
      * @var string 支付宝服务器主动通知商户服务器里指定的页面http/https路径。
      */
-    public $notify_url;
-
-    /**
-     * @var AopClient
-     */
-    public $instance;
+    public $notifyUrl;
 
     /**
      * @var string 商户网站唯一订单号。由商家自定义，64个字符以内，仅支持字母、数字、下划线且需保证在商户端不重复。
      */
-    public $out_trade_no;
+    public $outTradeNo;
 
     /**
      * @var array 订单总金额，单位为元，精确到小数点后两位，取值范围[0.01,100000000]，金额不能为0
      */
-    public $total_amount = 0.00;
+    public $totalAmount = 0.00;
 
     /**
      * @var string 订单标题。注意：不可使用特殊字符，如 /，=，& 等。
@@ -84,6 +83,18 @@ class AliPay extends Component
      */
     public $trade_no;
 
+    // ------ 以下为SDK运行中的参数 ------ /
+
+    /**
+     * @var AopClient
+     */
+    public $instance;
+
+    /**
+     * @var string 支付渠道，一般指的是支付宝支付客户端类型 APP:App
+     */
+    public $payChannel = 'App';
+
     /**
      * @var array 错误信息
      */
@@ -94,20 +105,22 @@ class AliPay extends Component
      *
      * @author Bowen
      * @email bowen@jiuchet.com
-     * @param string $type 支付宝支付客户端类型 APP:App
+     * @param string $channel 支付宝支付客户端类型 APP:App
      * @return $this
      * @lasttime 2022/11/10 01:12
      */
-    public function build(string $type = 'App'): AliPay
+    public function build(string $channel = 'App'): AliPay
     {
-        $this->appId                  = Yii::$app->params[$type . "Config"]['alipay']['app_id'];
-        $this->rsaPrivateKeyFile      = Yii::getAlias(Yii::$app->params[$type . "Config"]['alipay']['rsaPrivateKeyFile'] ?? "@common/pay/alipay/$this->appId/rsa_private_key.txt");
-        $this->alipayRsaPublicKeyFile = Yii::getAlias(Yii::$app->params[$type . "Config"]['alipay']['alipayRsaPublicKeyFile'] ?? "@common/pay/alipay/$this->appId/alipay_rsa_public_key.txt");
+        $this->payChannel = Safe::gpcBelong($channel, ['App'], 'App');
+
+        $this->appId                  = Yii::$app->params[$this->payChannel . "Config"]['alipay']['app_id'];
+        $this->rsaPrivateKeyFile      = Yii::getAlias(Yii::$app->params[$this->payChannel . "Config"]['alipay']['rsaPrivateKeyFile'] ?? "@common/pay/alipay/$this->appId/rsa_private_key.txt");
+        $this->alipayRsaPublicKeyFile = Yii::getAlias(Yii::$app->params[$this->payChannel . "Config"]['alipay']['alipayRsaPublicKeyFile'] ?? "@common/pay/alipay/$this->appId/alipay_rsa_public_key.txt");
         if (!file_exists($this->rsaPrivateKeyFile)) {
             $this->errors[] = '应用私钥文件不存在';
             return $this;
         }
-        $this->notify_url = Yii::$app->params[$type . "Config"]['alipay']['notifyUrl'];
+        $this->notifyUrl = Yii::$app->params[$channel . "Config"]['alipay']['notifyUrl'];
 
         // 获取应用私钥和支付宝公钥
         $rsaPrivateKey      = file_get_contents($this->rsaPrivateKeyFile);
@@ -138,13 +151,13 @@ class AliPay extends Component
      * @return $this
      * @lasttime 2022/11/10 01:15
      */
-    public function total_amount($total = 0): AliPay
+    public function totalAmount($total = 0): AliPay
     {
         // 如果不包含浮点数，意味着是整数，需要转换为元
         if (Util::strExists($total, '.') === false) {
-            $this->total_amount = bcdiv($total, 100, 2);
+            $this->totalAmount = bcdiv($total, 100, 2);
         } else {
-            $this->total_amount = Util::round_money($total);
+            $this->totalAmount = Util::round_money($total);
         }
 
         return $this;
@@ -176,7 +189,7 @@ class AliPay extends Component
      */
     public function outTradeNo(string $outTradeNo = null): AliPay
     {
-        $this->out_trade_no = $outTradeNo;
+        $this->outTradeNo = $outTradeNo;
         return $this;
     }
 
@@ -191,7 +204,7 @@ class AliPay extends Component
      */
     public function notifyUrl(string $notifyUrl = ''): AliPay
     {
-        $this->notify_url = $notifyUrl;
+        $this->notifyUrl = $notifyUrl;
         return $this;
     }
 
@@ -244,10 +257,10 @@ class AliPay extends Component
      */
     public function checkTransactionsError()
     {
-        if ($this->total_amount <= 0)
+        if ($this->totalAmount <= 0)
             $this->errors[] = '金额必须大于0';
 
-        if (!Util::strExists($this->total_amount, '.'))
+        if (!Util::strExists($this->totalAmount, '.'))
             $this->errors[] = '订单金额(单位元)必须为保留两位小数的浮点数';
 
         if (!empty($this->errors))
@@ -262,24 +275,24 @@ class AliPay extends Component
      * @author Bowen
      * @email bowen@jiuchet.com
      *
-     * @return array|mixed|true
+     * @return array
      * @lasttime: 2023/3/10 12:15
      */
-    public function APP()
+    private function APP(): array
     {
         $check = $this->checkTransactionsError();
         if (Util::isError($check))
             return $check;
 
         $object               = new stdClass();
-        $object->out_trade_no = $this->out_trade_no ?? 'jc' . date('YmdHis') . '000' . Util::random(4, true);
-        $object->total_amount = $this->total_amount;
+        $object->out_trade_no = $this->outTradeNo ?? 'jc' . date('YmdHis') . '000' . Util::random(4, true);
+        $object->total_amount = $this->totalAmount;
         $object->subject      = $this->subject ?? '商品' . date('YmdHis');
         $object->product_code = 'QUICK_MSECURITY_PAY'; // app支付需固定为此参数
 
         $json    = json_encode($object);
         $request = new AlipayTradeAppPayRequest();
-        $request->setNotifyUrl($this->notify_url ?: '');
+        $request->setNotifyUrl($this->notifyUrl ?: '');
         $request->setBizContent($json);
 
         $result = $this->instance->sdkExecute($request);
@@ -291,6 +304,152 @@ class AliPay extends Component
             if (isset($result->$responseNode->trade_no))
                 $this->trade_no = $result->$responseNode->trade_no;
 
+            return ArrayHelper::toArray($result);
+        } else {
+            return Util::error($resultCode, $result->$responseNode->sub_msg, ArrayHelper::toArray($result));
+        }
+    }
+
+    /**
+     * 进行支付下单
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @return array
+     * @lasttime: 2023/4/17 1:13 PM
+     */
+    public function pay(): array
+    {
+        $payChannel = $this->payChannel;
+        return $this->$payChannel();
+    }
+
+    /**
+     * 查询订单
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $trade_no 支付宝订单号，如果为空，则通过商户订单号查询
+     * @return array
+     * @throws \Exception
+     * @lasttime: 2023/4/17 1:27 PM
+     */
+    public function query(string $trade_no = ''): array
+    {
+        $trade_no = $trade_no ?: $this->trade_no;
+        if (empty($trade_no) && empty($this->outTradeNo))
+            return Util::error(ErrCode::PARAMETER_ERROR, 'trade_no和out_trade_no不能同时为空');
+
+        $object = new stdClass();
+        if (!empty($trade_no))
+            $object->trade_no = $trade_no;
+        if (empty($object->trade_no) && !empty($this->outTradeNo))
+            $object->out_trade_no = $this->outTradeNo;
+
+        $json    = json_encode($object);
+        $request = new AlipayTradeQueryRequest();
+        $request->setBizContent($json);
+        $result = $this->instance->execute($request);
+
+        $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+        $resultCode   = $result->$responseNode->code;
+        if (!empty($resultCode) && $resultCode == 10000) {
+            return ArrayHelper::toArray($result);
+        } else {
+            return Util::error($resultCode, $result->$responseNode->sub_msg, ArrayHelper::toArray($result));
+        }
+    }
+
+    /**
+     * 关闭订单
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param string $trade_no 支付宝订单号，如果为空，则通过商户订单号查询
+     * @return array
+     * @throws \Exception
+     * @lasttime: 2023/4/17 1:31 PM
+     */
+    public function close(string $trade_no = ''): array
+    {
+        $trade_no = $trade_no ?: $this->trade_no;
+        if (empty($trade_no) && empty($this->outTradeNo))
+            return Util::error(ErrCode::PARAMETER_ERROR, 'trade_no和out_trade_no不能同时为空');
+
+        $object = new stdClass();
+        if (!empty($trade_no))
+            $object->trade_no = $trade_no;
+        if (empty($object->trade_no) && !empty($this->outTradeNo))
+            $object->out_trade_no = $this->outTradeNo;
+
+        $json    = json_encode($object);
+        $request = new AlipayTradeCloseRequest();
+        $request->setBizContent($json);
+        $result = $this->instance->execute($request);
+
+        $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+        $resultCode   = $result->$responseNode->code;
+        if (!empty($resultCode) && $resultCode == 10000) {
+            return ArrayHelper::toArray($result);
+        } else {
+            return Util::error($resultCode, $result->$responseNode->sub_msg, ArrayHelper::toArray($result));
+        }
+    }
+
+    /**
+     * 申请退款
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param float $refundAmount 退款金额（单位元）。需要退款的金额，该金额不能大于订单金额，单位为元，支持两位小数。注：如果正向交易使用了营销，该退款金额包含营销金额，支付宝会按业务规则分配营销和买家自有资金分别退多少，默认优先退买家的自有资金。如交易总金额100元，用户支付时使用了80元自有资金和20元无资金流的营销券，商家实际收款80元。如果首次请求退款60元，则60元全部从商家收款资金扣除退回给用户自有资产；如果再请求退款40元，则从商家收款资金扣除20元退回用户资产以及把20元的营销券退回给用户（券是否可再使用取决于券的规则配置）。
+     * @param float $totalAmount 订单总金额（单位元）
+     * @param string|null $refundReason 退款原因说明。商家自定义，将在会在商户和用户的pc退款账单详情中展示
+     * @param string|null $outRequestNo 退款请求号。标识一次退款请求，需要保证在交易号下唯一，如需部分退款，则此参数必传。注：针对同一次退款请求，如果调用接口失败或异常了，重试时需要保证退款请求号不能变更，防止该笔交易重复退款。支付宝会保证同样的退款请求号多次请求只会退一次。
+     * @param string $trade_no 支付宝交易号。和商户订单号 out_trade_no 不能同时为空。
+     * @return array
+     * @throws \Exception
+     * @lasttime: 2023/4/17 1:58 PM
+     */
+    public function refund(float $refundAmount, float $totalAmount = 0.00, ?string $refundReason = '', ?string $outRequestNo = null, string $trade_no = ''): array
+    {
+        if ($refundAmount <= 0)
+            return Util::error(ErrCode::PARAMETER_ERROR, '退款金额不能为空');
+
+        if ($totalAmount > 0) {
+            if ($refundAmount > $totalAmount)
+                return Util::error(ErrCode::PARAMETER_ERROR, '退款金额不能大于订单金额');
+            if ($refundAmount < $totalAmount && empty($outRequestNo))
+                return Util::error(ErrCode::PARAMETER_ERROR, '部分退款时，退款请求号不能为空');
+        }
+
+        $trade_no = $trade_no ?: $this->trade_no;
+        if (empty($trade_no) && empty($this->outTradeNo))
+            return Util::error(ErrCode::PARAMETER_ERROR, 'trade_no和out_trade_no不能同时为空');
+
+        $object = new stdClass();
+        if (!empty($trade_no))
+            $object->trade_no = $trade_no;
+        if (empty($object->trade_no) && !empty($this->outTradeNo))
+            $object->out_trade_no = $this->outTradeNo;
+
+        $object->refund_amount = $refundAmount;
+        if (!empty($refundReason))
+            $object->refund_reason = $refundReason;
+        if (!empty($outRequestNo))
+            $object->out_request_no = $outRequestNo;
+
+        $json    = json_encode($object);
+        $request = new AlipayTradeRefundRequest();
+        $request->setBizContent($json);
+        $result = $this->instance->execute($request);
+
+        $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
+        $resultCode   = $result->$responseNode->code;
+        if (!empty($resultCode) && $resultCode == 10000) {
             return ArrayHelper::toArray($result);
         } else {
             return Util::error($resultCode, $result->$responseNode->sub_msg, ArrayHelper::toArray($result));
