53bb5ba4097e8e243879bddd6ed4b012804c6ac6
jcbowen <bowen@jiuchet.com>
2024-07-24 15:26:00 +0800

    新增 批量设置对象属性方法

--- src/base/Component.php
+++ src/base/Component.php
@@ -18,9 +18,34 @@ class Component extends \yii\base\Component
      */
     public function config(array $config = []): Component
     {
-        if (!empty($config))
-            Yii::configure($this, $config);
+        static::setProperties($this, $config);
 
         return $this;
+    }
+
+    /**
+     * 批量设置对象属性
+     *
+     * @author Bowen
+     * @email bowen@jiuchet.com
+     *
+     * @param object $object 要设置的对象
+     * @param array $properties 要设置的属性数组
+     * @return object 设置后的对象
+     *
+     * @lasttime: 2024/7/24 下午3:22
+     */
+    public static function setProperties(object $object, array $properties): object
+    {
+        foreach ($properties as $name => $value) {
+            // 检查属性是否存在
+            if (property_exists($object, $name) || method_exists($object, '__set')) {
+                $object->$name = $value;
+            } else {
+                Yii::error('Property "' . $name . '" does not exist in class "' . get_class($object) . '"', __METHOD__);
+            }
+        }
+
+        return $object;
     }
 }
